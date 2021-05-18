e51625f7e5e7f72829009d0fe30a40cb6a89aecd
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-05-19 01:09:28 +0700

    Create main.py

---
+++ YouTube/main.py
@@ -0,0 +1,85 @@
+from user import User
+from videos import Video
+from channel import Channel
+
+def main():
+    channel = Channel()
+
+    print("welcome to youtube.")
+
+    app = "open"
+
+    while app == "open":
+        identity = input("\nlogin as user or creator?\n").lower()
+
+        if identity == "user":
+            username = input("What is your name? ")
+            username = User(username)
+            print("\nwelcome " + username.name + "\n")
+            print(username.user_menu())
+
+            while identity == "user":
+                action = input("what do you want to do? (view/logout/exit)\n").lower()
+
+                if action == "view":   
+                    if len(channel.videos) != 0:
+                        print("\nVideos available:\n")
+                        for vid in channel.videos:
+                            print(f"- {vid.name} \n")
+                            choice = input("What do you want to watch? (video name)\n")
+                            print(channel.view_video(choice))
+                    
+                    else:
+                        print("We're sorry there are no videos at the moment. \n")
+
+                elif action == "logout":
+                    identity = "undefined"
+                    break
+
+                elif action == "exit":
+                    print("Thank you for using Youtube")
+                    app = "close"
+                    break
+                    
+    
+        if identity == "creator":
+            if channel.name == "":
+                channel_name = input("What do you want to name your channel?\n")
+                channel.name_channel(channel_name)
+
+            print("welcome , " + channel.name +"\n")
+
+            print(channel.owner_menu())
+
+            while identity == "creator":
+                action = input("What do you want to do? (add, views, list, logout, exit) \n")
+
+                if action == "add":
+                    vidname = input("What is the name of the video? \n")
+                    print(channel.make_video(vidname))
+
+                elif action == "views":
+                    if len(channel.videos) != 0:
+                        print("\nVideos:\n")
+                        for vid in channel.videos:
+                            print(f"- {vid.name} - {vid.views} views\n")
+                    else:
+                        print("No videos found. \n")
+
+                elif action == "list":
+                    if len(channel.videos) != 0:
+                        print("\nVideos:\n")
+                        for vid in channel.videos:
+                            print(f"- {vid.name} \n")
+                    else:
+                        print("No videos found. \n")
+
+                elif action == "logout":
+                    identity = "undefined"
+                    continue
+
+                elif action == "exit":
+                    print("Thank you for using Youtube")
+                    app = "close"
+                    break
+main()
